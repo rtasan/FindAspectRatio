97abf8384a80232ca91d9a101301363b2e132f5e
rtasan <42664466+rtasan@users.noreply.github.com>
2021-02-23 16:12:50 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import wx
+from MyProject1MainChild import MyProject1MainChild
+
+if __name__ == '__main__':
+	app = wx.App(False)
+	frame = MyProject1MainChild(None)
+	frame.Show(True)
+	app.MainLoop()
